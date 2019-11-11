281d6dc87c84eef00974ecbeec8f2f1742d65b9b
stef-codes <stefcodes@gmail.com>
2019-11-11 12:25:14 -0800

    add model

--- Rakefile
+++ Rakefile
@@ -11,9 +11,9 @@ end
 
 task :scrape_rooms do 
     #instantiate a scraper and have it find new rooms 
-    nyc_scraper = RoomScraper.new('https://newyork.craigslist.org/search/roo').update_rooms
+    nyc_scraper = RoomScraper.new('https://newyork.craigslist.org/search/roo')
     # nyc_scraper.call
-    # chicago_scraper = RoomScraper.new(https://chicago.craigslist.org/search/roo).update_rooms
+    # chicago_scraper = RoomScraper.new(https://chicago.craigslist.org/search/roo)
     # chicago_scraper.call
     #after this method call I should be able to say Room.all and have rooms there
 end 
--- lib/room_scraper.rb
+++ lib/room_scraper.rb
@@ -2,18 +2,18 @@ class RoomScraper
 
     def initialize(index_url)
       @doc = Nokogiri::HTML(open(index_url))
-        binding.pry
     end 
 
     def call 
         rows.each do |row_doc|
-            scrape_row(row_doc)
+           room = Room.new_from_hash(scrape_row(row_doc))
+            room.save #=> should put the room in my database
         end 
     end 
 
    private
     def rows
-        @rows || = @doc.css(".result-row")
+        @rows ||= @doc.css(".result-row")
         # @doc.search("div.content span.rows p.row") --doesn't work
         # @doc.search("span.pl time")
     #   @doc.css(".result-date")  -- works
@@ -25,7 +25,9 @@ class RoomScraper
             :date_created => row.search("time").attr("datetime").text,
             :title => row.search("a.hdrlnk").text,
             :url => row.search("a.hdrlnk").attr("href").text,
-            :price => row.css(".result-price").text
+            :price => row.search("span.price")[0].text
+            # css(".result-price")[0].text
+            # search("span.result-price")[0].text
         }
     end
 end 
